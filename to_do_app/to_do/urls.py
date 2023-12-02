7746e2ec639198da79332ef0706d146b6f482ae6
Antoni Łubisz <lanti222@gmail.com>
2023-12-02 19:56:02 +0100

    commit

--- to_do_app/to_do/urls.py
+++ to_do_app/to_do/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
+from django.contrib.auth.views import LogoutView
 from . import views
 
 urlpatterns = [
     path("register", views.RegisterView.as_view(), name="register"),
     path("login", views.LoginView.as_view(), name="login"),
-    path("logout", views.user_logout, name="logout"),
-    path("remove/<int:id>",  views.remove_task, name="remove"),
+    path("logout", LogoutView.as_view(next_page="login"), name="logout"),
+    path("remove/<int:id>",  views.RemoveTaskView.as_view(), name="remove"),
     path("", views.IndexView.as_view(), name="index"),
 ]
--- to_do_app/to_do/views.py
+++ to_do_app/to_do/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, redirect
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 from django.views import View
 from .forms import RegisterForm, TaskForm
-from django.contrib.auth import login, logout
-from django.contrib.auth.forms import AuthenticationForm
+from django.views.generic.list import ListView
+from django.contrib.auth import login
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import LoginView
+from django.contrib import messages
 from .models import Task
 
 # # Create your views here.
@@ -26,29 +28,30 @@ class RegisterView(View):
         return render(request, 'to_do/register.html', {'form': form})
 
 
-class LoginView(View):
-    def get(self, request):
-        form = AuthenticationForm()
-        return render(request, 'to_do/login.html', {'form': form})
+class LoginView(LoginView):
+    template_name = "to_do/login.html"
+    redirect_authenticated_user = True
 
-    def post(self, request):
-        form = AuthenticationForm(request, data=request.POST)
+    def get_success_url(self):
+        return reverse_lazy('index')
 
-        if form.is_valid():
-            user = form.get_user()
-            login(request, user)
-            return redirect(reverse('index'))
-
-        return render(request, 'to_do/login.html', {'form': form})
+    def form_invalid(self, form):
+        messages.error(self.request, 'Invalid username or password')
+        return self.render_to_response(self.get_context_data(form=form))
 
 
-class IndexView(LoginRequiredMixin, View):
+class IndexView(LoginRequiredMixin, ListView):
     login_url = "login"
+    context_object_name = "tasks"
+    template_name = "to_do/index.html"
 
-    def get(self, request):
-        form = TaskForm()
-        user_tasks = Task.objects.filter(user=request.user)
-        return render(request, "to_do/index.html", {"tasks": user_tasks, "form": form})
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["form"] = TaskForm()
+        return context
+
+    def get_queryset(self):
+        return Task.objects.filter(user=self.request.user)
 
     def post(self, request):
         form = TaskForm(request.POST)
@@ -62,12 +65,10 @@ class IndexView(LoginRequiredMixin, View):
             return render(request, "to_do/index.html", {"tasks": user_tasks, "form": form})
 
 
-def user_logout(request):
-    logout(request)
-    return redirect(reverse("login"))
+class RemoveTaskView(LoginRequiredMixin, View):
+    login_url = "login"
 
-
-def remove_task(request, id):
-    task = Task.objects.get(id=id)
-    task.delete()
-    return redirect(reverse("index"))
+    def get(self, request, id):
+        task = Task.objects.get(id=id)
+        task.delete()
+        return redirect(reverse("index"))
